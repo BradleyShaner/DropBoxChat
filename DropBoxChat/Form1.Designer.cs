f53b551775ac3b8058abbda67f975c0a2c7a5316
LostSoulFly <bradley.shaner@gmail.com>
2017-04-14 23:06:53 -0400

    Learning C#
    It works! It's not perfect, but it works for small chats!

---
+++ DropBoxChat/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DropBoxChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textChat = new System.Windows.Forms.TextBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textChat
+            // 
+            this.textChat.Location = new System.Drawing.Point(12, 12);
+            this.textChat.Multiline = true;
+            this.textChat.Name = "textChat";
+            this.textChat.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textChat.Size = new System.Drawing.Size(583, 323);
+            this.textChat.TabIndex = 0;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(13, 342);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(582, 20);
+            this.textInput.TabIndex = 1;
+            this.textInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textInput_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 369);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.textChat);
+            this.Name = "Form1";
+            this.Text = "DropBox Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textChat;
+        private System.Windows.Forms.TextBox textInput;
+    }
+}
+
---
+++ DropBoxChat/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DropBoxChat
+{
+    public partial class Form1 : Form
+    {
+        delegate void TextForChat(string text);
+
+        public Form1()
+        {
+            InitializeComponent();
+            textChat.ReadOnly = true;
+            //ThreadStart readChat = new ThreadStart(ReadChatFile);
+            
+            Task readChat = Task.Run(() => ReadChatFile());
+
+            
+        }
+
+        private void textInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SetChat(string text)
+        {
+            if (this.textChat.InvokeRequired)
+            {
+                TextForChat t = new TextForChat(SetChat);
+                this.Invoke(t, new object[] { text });
+            }
+            else
+            {
+                textChat.Text = text;
+            }
+        }
+
+        private void textInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return) {
+                //textChat.Text += textInput.Text + Environment.NewLine;
+                using (StreamWriter file = File.AppendText(@"chat.txt"))
+                    file.WriteLine(Environment.UserName + ": " + textInput.Text);
+                    textInput.Clear();
+                }
+        }
+        
+        public void ReadChatFile()
+        {
+
+            //this.SetChat("test");
+            while (true)
+                {
+                string text = System.IO.File.ReadAllText(@"chat.txt");
+                this.SetChat(text);
+                Thread.Sleep(2000);
+                }
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
